d1289a9b28894f78cff55b0579df078d9d072941
Marwen2020 <marwen.hella@gmail.com>
2023-05-04 20:23:23 +0200

    Add a Panel

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,12 +1,12 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/ui/model/json/JSONModel",
-    "sap/ui/model/FilterOperator"
+    "sap/ui/model/json/JSONModel"
+    //"sap/ui/model/FilterOperator"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller, JSONModel, FilterOperator ) {
+    function (Controller, JSONModel ) {
         "use strict";
 
         return Controller.extend("code.demo.training.controller.View1", {
@@ -19,21 +19,28 @@ sap.ui.define([
                
 
             },  
-            onSubmit : function(oEvent) { 
-                
-
+            onItemSelected : function(oEvent,index) { 
+                var sPath = oEvent.getSource().getBindingContext().getPath()
+                var oPanel = this.byId("Panel")
+                oPanel.bindElement(sPath)
+                oPanel.setVisible()
+                var oImage = this.byId("Image")
+                var oModel = this.getOwnerComponent().getModel()
             },
             onSearch : function(oEvent){ 
                 var aFilter = []
-                var filterValue = oEvent.getParameter("selectionSet")[0].getValue(); 
-                if (filterValue) { 
-                    var filter = new sap.ui.model.Filter("ProductName" , FilterOperator.EQ , filterValue)
+                var filterValue1 = oEvent.getParameter("selectionSet")[0].getValue(); 
+                var filterValue2= oEvent.getParameter("selectionSet")[1].getValue(); 
+                if (filterValue1) { 
+                    var filter = new sap.ui.model.Filter("ProductName" , "EQ"  , filterValue1)
+                    aFilter.push(filter)
+                }
+                if (filterValue2) { 
+                    var filter = new sap.ui.model.Filter("Category/CategoryName" , "EQ"  , filterValue2)
                     aFilter.push(filter)
                 }
                 var oList = this.byId("List").getBinding("items")
                 oList.filter(aFilter)
-              
-                
             }
         });
     });
